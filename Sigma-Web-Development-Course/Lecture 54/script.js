e85075f0e71d81b6be4811e2fe958afe60f89681
vinay-021 <vinaysh021@gmail.com>
2025-09-06 17:13:48 +0530

    08092025

---
+++ Sigma-Web-Development-Course/Lecture 54/script.js
@@ -0,0 +1,20 @@
+alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Do you want to leave this site?")
+
+if (isTrue) {
+    console.log("You are leaving this site")
+} else {
+    console.log("Continuing to this site")
+}
+
+console.log("Your name is " + a)
+
+document.title="Hey I am good"
+
+document.body.style.backgroundColor="yellow"
---
+++ Sigma-Web-Development-Course/Lecture 55/script.js
@@ -0,0 +1,8 @@
+console.log("Hey! Welcome to Lecture 55")
+
+var a=5;
+var b=6;
+var c="Vinay";
+
+console.log(a+b+8);
+console.log(typeof a, typeof b, typeof c);
